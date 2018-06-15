fb876d6323d6f610b347f7928cc11a4d6abfea6d
nithin-agiratech <nithin@agiratech.com>
2018-06-15 17:36:32 +0530

    browser stack details added for testing with bs

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,13 +5,21 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 60000,
+  seleniumAddress: 'http://hub-cloud.browserstack.com/wd/hub',
   specs: [
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    'os': 'windows',
+    'os_version': '8.1',
+    'browserstack.debug': 'true',
+    'browserstack.networkLogs': 'true',
+    'browserstack.local' : 'true',
+    'browserstack.user' : '',
+    'browserstack.key' : ''
   },
-  directConnect: true,
+  directConnect: false,
   baseUrl: 'http://localhost:4202',
   framework: 'jasmine',
   jasmineNodeOpts: {
